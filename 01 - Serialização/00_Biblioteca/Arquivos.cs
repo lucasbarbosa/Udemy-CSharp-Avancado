d8c4732271b4b7723472abc1d01e4f4f21f9d488
Lucas Barbosa <lucasbtsilva@gmail.com>
2021-03-08 18:33:40 -0300

    Update

--- 01 - Serialização/00_Biblioteca/Arquivos.cs
+++ 01 - Serialização/00_Biblioteca/Arquivos.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace Biblioteca
 {
@@ -6,10 +7,12 @@ namespace Biblioteca
     {
         public static string CaminhoBase()
         {
-            var path = @"C:\Lucas.Teixeira.Files\TestsProjects\Udemy\CSharpAvancado\_Arquivos\";
             // var path = @"C:\Estudos\Dotnet\Udemy\CSharpAvancado\_Arquivos\";
-			
-			return path;
+            var path = Path.Combine(
+                            Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, 
+                            @"Arquivos\");
+
+            return path;
         }
 
         public static StreamReader ArquivoLer(string arquivo)
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Udemy - C# Avançado
-Projeto do curso: https://www.udemy.com/linguagem-de-programacao-c-avancado/
+
+App de estudo do curso C# Avançado | Udemy
+
+### Link do curso
+* https://www.udemy.com/linguagem-de-programacao-c-avancado/
+
+#### Instrutor
+* Elias Ribeiro Da Silva Costa
+
+#### Módulos
+* Seção 1: Sobre o curso
+* Seção 2: Serialização
+* Seção 3: Tópicos avançados
+* Seção 4: LINQ e LAMBDA
+* Seção 5: Delegate e Eventos
+* Seção 6: Thread - Execução em Paralelo
+* Seção 7: Task
+* Seção 8: Attributos e Reflections
+* Seção 9: Nuget - Crie seu pacote
+* Seção 10: Downloads
+* Seção 11: Final
